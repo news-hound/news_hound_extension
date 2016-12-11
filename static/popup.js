039a5bb2fbd492e7a35a9513624c8cb95ad21e83
Jeremy Thibeaux <jthibeaux@gmail.com>
2016-12-11 13:15:52 -0800

    popup overhaul

--- static/popup.js
+++ static/popup.js
@@ -1,19 +1,33 @@
+function checkSetting(key) {
+  return localStorage.getItem(key) == 'true'
+}
+
 $( document ).ready(function() {
 
-  $("#page-body").load("filters.html");
+  $('#inputRowCheckbox1').change(function() {
+    localStorage.setItem("newshound", this.checked);
+  })
+  .prop('checked', checkSetting('newshound'));
 
-  $('#inputRowCheckbox1').click(function() {
-    localStorage.setItem("ai_mode", true);
-    localStorage.setItem("todos", false);
-  });
+  $('#inputRowCheckbox2').change(function() {
+    localStorage.setItem("snopes", this.checked);
+  })
+  .prop('checked', checkSetting('snopes'));
+
+  $('#inputRowCheckbox3').change(function() {
+    localStorage.setItem("real_or_satire", this.checked);
+  })
+  .prop('checked', checkSetting('real_or_satire'));
+
+  $('#inputRowCheckbox4').change(function() {
+    localStorage.setItem("politifact", this.checked);
+  })
+  .prop('checked', checkSetting('politifact'));
 
   $('#openContactUs').click(function() {
     var win = window.open("https://htmlpreview.github.io/?https://github.com/madhurxyz/Ronin/blob/master/index.html#contact", '_blank');
     win.focus();
   });
 
-  $('#showServices').click(function() {
-    $("#page-body").load("section.html");
-  });
-
+  console.log('done');
 });
